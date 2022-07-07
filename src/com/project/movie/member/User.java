a6f99a013d986ac8f9db3a902edc14c51a2722cd
samweol <sejuyes@gmail.com>
2022-07-07 23:15:53 +0900

    공통 : project 수정 완료

--- src/com/project/movie/member/User.java
+++ src/com/project/movie/member/User.java
@@ -1,32 +1,5 @@
 package com.project.movie.member;
 
-import com.project.movie.DBUtil;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
 public class User {
-    public static void main(String[] args) {
-        try {
 
-            Connection conn;
-            conn = DBUtil.open();
-            Statement stat;
-            ResultSet rs;
-
-            String sql = "select * from tblUser";
-
-            stat = conn.createStatement();
-            rs = stat.executeQuery(sql);
-
-            while(rs.next()) {
-                System.out.println(rs.getString("id"));
-            }
-
-
-        } catch (Exception e) {
-
-        }
-    }
 }
