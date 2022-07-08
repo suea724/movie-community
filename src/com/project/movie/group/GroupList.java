a8bf155eaa40c085fd1d3ea921d6dc8487e6f08c
chanjin5212 <chanjin5212@gmail.com>
2022-07-08 18:28:35 +0900

    찬진 : style, template 수정

--- src/com/project/movie/group/GroupList.java
+++ src/com/project/movie/group/GroupList.java
@@ -1,4 +1,24 @@
 package com.project.movie.group;
 
-public class GroupList {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/group/grouplist.do")
+public class GroupList extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/group/grouplist.jsp");
+        dispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
