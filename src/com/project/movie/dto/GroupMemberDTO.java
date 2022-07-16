d7932c19dd53ea2fe58f4af671de9915d8bec92a
rladudtjr871 <rladudtjr871@naver.com>
2022-07-16 22:50:40 +0900

    진짜 최종

--- src/com/project/movie/dto/GroupMemberDTO.java
+++ src/com/project/movie/dto/GroupMemberDTO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 public class GroupMemberDTO {
 
     private int seq;
+    private String id;
     private String name;
     private String nickname;
     private String postcnt;
